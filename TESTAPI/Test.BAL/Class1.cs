8650225f53e63dca8a499d5ede59eb3e08a8edc9
KolliNikhil05 <156987025+KolliNikhil05@users.noreply.github.com>
2024-01-20 18:03:29 +0530

    Add files via upload

---
+++ TESTAPI/Test.BAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test.BAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ TESTAPI/Test.Business.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Test.Business.Contracts
+{
+    public class Class1
+    {
+    }
+}
