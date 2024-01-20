74bc56e2f0ae494b087ab32b12ad68ed23fbaa93
KolliNikhil05 <156987025+KolliNikhil05@users.noreply.github.com>
2024-01-20 17:19:15 +0530

    Add files via upload

---
+++ DataBind/views/WebForm1.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DataBind.views.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body
+        {
+            background-color:aquamarine;
+            padding:10px;
+            text-align:center
+        }
+        .txtbox1
+        {
+            padding:10px;
+        }
+        .txtbox2
+        {
+            padding:10px;
+        }
+    </style>
+</head>
+<body>  
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblException" runat="server" Text=""></asp:Label>
+            <asp:GridView ID="grdPeople" runat="server"></asp:GridView><br />
+            <asp:GridView ID="grdPeoples" runat="server"></asp:GridView>
+        </div>
+        <div class="txtbox1">
+            <asp:Label ID="label1" runat="server" Text="Id:"></asp:Label>
+            <asp:TextBox ID="textbox1" runat="server" Text=""></asp:TextBox>
+       </div>
+        <div class="txtbox2">
+            <asp:Label ID="label2" runat="server" Text="Name:"></asp:Label>
+            <asp:TextBox ID="textbox2" runat="server" Text=""></asp:TextBox>
+         </div>
+        <div class="txtbox3">
+            <asp:Label ID="label3" runat="server" Text="Age:"></asp:Label>
+            <asp:TextBox ID="textbox3" runat="server" Text=""></asp:TextBox>
+        </div>
+        <br />
+        <%--<div class="dropdown">
+            <asp:Label ID="label4" runat="server" Text="select"></asp:Label>
+            <asp:DropDownList ID="drd" runat="server" OnSelectedIndexChanged="drd_SelectedIndexChanged"></asp:DropDownList>
+        </div>--%>
+        <div class="button">
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="button_click"></asp:Button>
+        </div>
+
+            
+       
+    </form>
+</body>
+</html>
---
+++ DataBind/views/WebForm1.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace DataBind.views
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //protected void Page_Load(object sender, EventArgs e)
+        //{
+        //    list();
+        //}
+
+        //public void list()
+        //{
+        //    DataSet ds = new DataSet();
+        //    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_people"].ConnectionString);
+        //    con.Open();
+        //    SqlCommand cmd = con.CreateCommand();
+        //    cmd.CommandText = "dropdown";
+        //    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        //    SqlDataAdapter ad = new SqlDataAdapter(cmd);
+        //    ad.Fill(ds);
+        //    drd.DataSource = ds.Tables[0];
+        //    drd.DataTextField = ds.Tables[0].Columns["Name"].ColumnName.ToString();
+        //    drd.DataBind();
+        //    con.Close();
+        //}
+
+        //protected void drd_SelectedIndexChanged(object sender, EventArgs e)
+        //{
+        //    string selectedValue = drd.SelectedValue;
+        //}
+
+        //protected void button_click(object sender, EventArgs e)
+        //{
+        //    DataTable dt = new DataTable();
+        //    try
+        //    {
+        //        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_people"].ConnectionString);
+        //        con.Open();
+        //        SqlCommand cmd = con.CreateCommand();
+        //        cmd.CommandText = "getdata";
+        //        cmd.Parameters.AddWithValue("@TheId", textbox1.Text);
+        //        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        //        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        //        da.Fill(dt);
+        //        textbox2.Text = dt.Rows[0]["Name"].ToString();
+        //        textbox3.Text = dt.Rows[0]["Address"].ToString();
+        //    }
+
+        //    catch(Exception ex) 
+        //    {
+        //        lblException.Text = ex.Message;
+        //    }
+
+        //}
+
+
+        protected void button_click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connection_people"].ConnectionString);
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "insertdata";
+            cmd.Parameters.AddWithValue("@Id", textbox1.Text);
+            cmd.Parameters.AddWithValue("@Name", textbox2.Text);
+            cmd.Parameters.AddWithValue("@Address", textbox3.Text);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+        }
+
+
+    }
+}
+
+
+
