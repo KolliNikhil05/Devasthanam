105266889f8d8ebec994dca60977cda09af80cf5
KolliNikhil05 <156987025+KolliNikhil05@users.noreply.github.com>
2024-01-20 17:04:08 +0530

    Add files via upload

---
+++ Devasthanam.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Devasthanam.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
