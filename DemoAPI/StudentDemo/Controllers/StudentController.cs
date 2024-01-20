611ae4f8cfac6250098b6e3232db3e3cff5969bc
KolliNikhil05 <156987025+KolliNikhil05@users.noreply.github.com>
2024-01-20 17:41:41 +0530

    Add files via upload

---
+++ DemoAPI/StudentDemo/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using StudentDemo.Models;
+using StudentDemo.Services;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StudentDemo.Controllers
+{
+
+    public class StudentController : Controller
+    {
+        private readonly StudentServices _apiService;
+
+        public StudentController(StudentServices apiService)
+        {
+            _apiService = apiService;
+        }
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    var students = await _apiService.GetStudentData();
+        //    return View("Student", students);  
+        //}
+        public IActionResult Index()
+        {
+            var students = _apiService.GetStudentData().Result;  
+            return View("Student", students);
+        }
+
+
+
+
+
+    }
+}
---
+++ DemoAPI/StudentDemo/Models/StudentModel.cs
@@ -0,0 +1,13 @@
+﻿    using System.Collections.Generic;
+
+    namespace StudentDemo.Models
+    {
+        public class StudentModel
+        {
+
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public long Mobile_Number { get; set; }
+            public int Age { get; set; }
+        }
+    }
