ec8bb656c0f8a44974707567dca204794a4c3f4b
KolliNikhil05 <156987025+KolliNikhil05@users.noreply.github.com>
2024-01-20 16:51:29 +0530

    Add files via upload

---
+++ ConsumeWebService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data;
+using System.Net.Http.Headers;
+
+namespace ConsumeWebService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RecruiteeData.RecruiteeData obj = new RecruiteeData.RecruiteeData();
+            DataTable dtRec=obj.GetRecruiteeData();
+
+            Console.WriteLine(dtRec.Rows.Count);
+
+        }
+    }
+}
